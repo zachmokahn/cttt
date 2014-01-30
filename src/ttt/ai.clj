c7d1db78f700d4ace3246a4fb76606f195b7d468
Zack Davy <zacharyjdavy@gmail.com>
2014-01-29 21:35:24 -0600

    Update ai.clj
    some of the language didn't read fluently

--- src/ttt/ai.clj
+++ src/ttt/ai.clj
@@ -6,20 +6,20 @@
 (declare minimax)
 
 (defn max-value [map]
-  "calculates the higest value from a map"
+  "discovers the higest value from a map"
   (apply max (vals map)))
 
 (defn alternate-max-value [coll]
-  "inverts the max value from a map"
+  "inverts the max value from a collection"
   (- (apply max coll)))
 
 (defn key-for [map value]
-  "gets the key value of the map corresponding to the higest value"
+  "gets the key values from a map corresponding to the given value"
   (filter #(= (get map %) value) (keys map)))
 
 (defn next-best-moves [board opponent depth]
-  "itterates through the minmax algorithm with the opponents token
-   to determine the next best moves that could be played"
+  "itterates through the minmax algorithm with the 'opponents' token
+   to determine the next best moves that can be played"
   (map #(minimax (move board % opponent) opponent (inc depth))
         (best-moves board opponent)))
 
@@ -31,7 +31,7 @@
 
 (defn determine-score [board turn opponent depth]
   "gives value to the outcome of the board based on the number
-   of turns it takes to reach that outcome"
+   of turns it take to be resolved"
   (if (win? board turn) (- 10 depth)
   (if (win? board opponent) (- 0 10 depth) 0)))
 
@@ -41,28 +41,27 @@
 
 (defn all-moves [board turn]
   "generates a map where the key represents the index of
-   the position and the value represents the score awarded
-   to that position"
+   the position with it's coresponding score"
   (let [spaces (empty-spaces board)]
   (zipmap spaces (board-scores board spaces turn))))
 
 (defn best-moves [board turn]
   "returns all of the keys that pertain to the highest score
-   relative to the player's token"
+   relative to the player"
   (let [scores (all-moves board turn)
         max-value (max-value scores)]
   (key-for scores max-value)))
 
 (defn minimax [board turn depth]
-  "either returns the point value for the move if the base case
-   is met or alternates the turn and recurs until a resolution
-   is reached"
+  "it either returns the point value for the move if the base case
+   is met (game over) or it alternates the turn and recurs until a 
+   resolution is met"
   (let [opponent (change-turn turn)]
     (if (game-over? board)
     (determine-score board turn opponent depth)
     (alternate-next-best-moves board opponent depth))))
 
 (defn best-move [board]
-  "randomly chooses any of the move that have the highest success
+  "randomly chooses any of the moves that have the highest success
    yield"
   (rand-nth (best-moves board :computer)))
